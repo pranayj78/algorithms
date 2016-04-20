1e79d0c4f0470ddb4b54c27fcdbb777f07d400d4
Nishant Shinde <nishantshinde@gmail.com>
2016-04-20 16:31:34 +0100

    Added a lowLevelToString() method

--- Algorithims/src/algorithims/MyInteger.java
+++ Algorithims/src/algorithims/MyInteger.java
@@ -5,6 +5,8 @@
  */
 package algorithims;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Pranay
@@ -14,6 +16,7 @@ public class MyInteger {
     public MyInteger(final int value){
         this.value = value;
     }
+    
     @Override
     public String toString(){
         long startTime = System.nanoTime();
@@ -43,10 +46,48 @@ public class MyInteger {
     }
     
     public static void main(String[] args){
-        MyInteger myInteger = new MyInteger(-125);
+        
+    	MyInteger myInteger = new MyInteger(-125);
         System.out.println("algorithims.MyInteger.main()" + myInteger);
-System.out.println("algorithims.MyInteger.main() "  +Integer.MAX_VALUE);
-System.out.println("algorithims.MyInteger.main() "  +Integer.MIN_VALUE);
+        System.out.println("algorithms.MyInteger.lowLevelToString()" + lowLevelToString(-125));
+        
+        System.out.println("algorithims.MyInteger.main() "  +Integer.MAX_VALUE);
+        System.out.println("algorithims.MyInteger.main() "  +Integer.MIN_VALUE);
 
+    }
+    
+    private static String lowLevelToString(int num) {
+    	
+    	long startTime = System.nanoTime();
+    	
+    	char[] characters = new char[12];
+    	int i;
+    	int character;
+    	char sign=' ';
+    	if(num<0) {
+    		sign='-';
+    		num=num*-1;
+    	}
+    	for(i=11;i>=0;i--) {
+    		if(num==0) {
+    			break;
+    		}
+    		character = num%10;
+    		if(character==0) {
+    			characters[i]= '0';
+    		} else {
+    			characters[i]= (char)('1'-1+character);
+    		}
+    		
+    		num=num/10;
+    	}
+    	characters[i]=sign;
+    	String returnValue = String.valueOf(characters, i, (12-i));
+    	
+        long endTime = System.nanoTime();
+        
+        System.out.println("Time taken (lowLevelToString) = " + (endTime - startTime));
+
+    	return returnValue;
     }
 }
