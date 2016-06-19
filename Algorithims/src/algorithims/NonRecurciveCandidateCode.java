ffb7f612eb1ba4d6badc91712ea41b75bb24c15e
Pranay <Pranay@Pranay-PC.Home>
2016-06-19 22:55:13 +0530

    added my integer class

---
+++ Algorithims/src/algorithims/NonRecurciveCandidateCode.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithims;
+
+/**
+ *
+ * @author Pranay
+ */
+public class NonRecurciveCandidateCode {
+     public static int passCount(int input1, int input2, int input3) {
+         
+         int totalPass = 0;
+         
+         return -1;
+     }
+}
---
+++ Algorithims/test/algorithims/MyIntegerTest.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithims;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pranay
+ */
+public class MyIntegerTest {
+    
+    public MyIntegerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of toString method, of class MyInteger.
+     */
+    @Test
+    public void testToStringMaxInt() {
+      System.out.println("toString for Max Int");
+          MyInteger instance = new MyInteger(Integer.MAX_VALUE);
+        String expResult = "" + Integer.MAX_VALUE;
+        String result = instance.toString();
+        assertEquals(expResult, result);
+       
+    }
+    
+     @Test
+    public void testToStringMinInt() {
+         System.out.println("toString for Min Int");
+        MyInteger instance = new MyInteger(Integer.MIN_VALUE);
+        String expResult = "" + Integer.MIN_VALUE;
+        String result = instance.toString();
+        assertEquals(expResult, result);
+       
+    }
+    
+     @Test
+    public void testToStringZero() {
+        System.out.println("toString for Zero");
+        MyInteger instance = new MyInteger(0);
+        String expResult = "" + 0;
+        String result = instance.toString();
+        assertEquals(expResult, result);
+       
+    }
+
+    @Test
+    public void testToString125Int() {
+        System.out.println("toString for 125");
+        MyInteger instance = new MyInteger(125);
+        String expResult = "" + 125;
+        String result = instance.toString();
+        assertEquals(expResult, result);
+       
+    }
+    
+     @Test
+    public void testToStringNegetive125Int() {
+        System.out.println("toString for Negetive 125");
+        MyInteger instance = new MyInteger(125);
+        String expResult = "" + 125;
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        
+    }
+    @Test
+    public void testToString() {
+        System.out.println("toString for Integer.toString");
+        Integer instance = Integer.MIN_VALUE;
+        String expResult = "" + Integer.MIN_VALUE;
+        long startTime = System.nanoTime();
+        String result = instance.toString();
+        long endTime = System.nanoTime();
+        System.out.println("algorithims.MyIntegerTest.testToString() " +(endTime - startTime));
+        assertEquals(expResult, result);
+        
+    }
+}
