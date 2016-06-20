e216f1a7193560275baf21ba404ac4be7d8e7c2d
Nishant Shinde <nishantshinde@gmail.com>
2016-06-20 12:01:06 +0100

    Added logging to SortProvider and fixed one bug

--- src/main/java/pranay78/algorithms/sorting/SortProvider.java
+++ src/main/java/pranay78/algorithms/sorting/SortProvider.java
@@ -15,49 +15,63 @@ import java.util.Arrays;
 public class SortProvider {
 
    public static void  quickSort(int[] dataToSort,int startIndex ,int endIndex){
-       if(dataToSort == null){
-           throw new NullPointerException(" Input Array cannot be null");
-       }
-    //   System.out.println("start index" +startIndex);
-     //  System.out.println("endIndex" +endIndex);
-       if(startIndex >= endIndex) {
-           return;
-       }
+	   
+		if (dataToSort == null) {
+			throw new NullPointerException(" Input Array cannot be null");
+		}
+		// System.out.println("start index" +startIndex);
+		// System.out.println("endIndex" +endIndex);
+		if (startIndex >= endIndex) {
+			return;
+		}
+
+		// chose the rightmost element
+		int pivotIndex = endIndex;
+		int leftIndex = startIndex;
+		int rightIndex = pivotIndex - 1;
+		boolean done = false;
+
+		   System.out.printf("quickSort() data:%s ; startIndex:%d ; endIndex:%d ; pivotIndex:%d \n",
+				   Arrays.toString(Arrays.copyOfRange(dataToSort, startIndex, endIndex+1)), startIndex, endIndex, pivotIndex);
+		   
+		while (!done) {
+			while ((rightIndex > leftIndex) && (dataToSort[leftIndex] <= dataToSort[pivotIndex])) {
+				leftIndex++;
+			}
+
+			while ((rightIndex > leftIndex) && (dataToSort[rightIndex] >= dataToSort[pivotIndex])) {
+				rightIndex--;
+			}
+			
+			if (rightIndex < leftIndex) {
+				done = true;
+			} else {
+				// int tempPIValue = dataToSort[pivotIndex];
+				int tempLeftValue = dataToSort[leftIndex];
+				int tempRightValue = dataToSort[rightIndex];
+				// dataToSort[pivotIndex] = tempLeftValue;
+				dataToSort[leftIndex] = tempRightValue;
+				dataToSort[rightIndex] = tempLeftValue;
+				// pivotIndex = rightIndex;
+				rightIndex--;
+				leftIndex++;
+			}
+
+		}
+		
+//		System.out.println("Before Pivot Insert:" + pivotIndex + " Result:" + Arrays.toString(dataToSort));
+		
+		leftIndex--;
+		int tempPIValue = dataToSort[pivotIndex];
+		dataToSort[pivotIndex] = dataToSort[leftIndex];
+		dataToSort[leftIndex] = tempPIValue;
+		
+		System.out.printf("After Pivot Insert Element: %d Result: %s \n", 
+				tempPIValue, Arrays.toString(Arrays.copyOfRange(dataToSort, startIndex, endIndex+1)));
+		
+		quickSort(dataToSort, startIndex, leftIndex - 1);
+		quickSort(dataToSort, leftIndex + 1, endIndex);
        
-       // chose the rightmost element
-       int pivotIndex = endIndex;
-       int leftIndex = startIndex;
-       int rightIndex = pivotIndex-1;
-       boolean done  = false;
-       while(!done){
-           while((rightIndex > leftIndex) && (dataToSort[leftIndex] <= dataToSort[pivotIndex])){
-               leftIndex++;
-           }
-           
-           while((rightIndex > leftIndex) && (dataToSort[rightIndex] >= dataToSort[pivotIndex])) {
-                rightIndex--;
-           }
-          if(rightIndex < leftIndex){
-              done = true;
-          }
-          else{
-          // int tempPIValue = dataToSort[pivotIndex];
-           int tempLeftValue = dataToSort[leftIndex];
-           int tempRightValue = dataToSort[rightIndex];
-          // dataToSort[pivotIndex] = tempLeftValue;
-           dataToSort[leftIndex] = tempRightValue;
-           dataToSort[rightIndex] = tempLeftValue;
-         //   pivotIndex = rightIndex;
-          rightIndex--;
-          leftIndex++;
-          }
-          int tempPIValue = dataToSort[pivotIndex];
-          dataToSort[pivotIndex] =  dataToSort[leftIndex];
-          dataToSort[leftIndex] = tempPIValue;
-                  
-       }
-       quickSort(dataToSort,startIndex,pivotIndex -1 );
-        quickSort(dataToSort, pivotIndex +1, endIndex);
    }
    
    public static void main(String[] args){
